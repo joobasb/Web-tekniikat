9c89915e39f4579b65e010d0245f28b416bb2da8
joobasb <aromaajoonas@gmail.com>
2022-05-20 15:37:24 +0300

    javascript updates

--- Bootstrap-webpage/javascript/javascript.js
+++ Bootstrap-webpage/javascript/javascript.js
@@ -1,22 +1,37 @@
 //Adding style to some bootstrap columns
 let aboutStyle = document.querySelector(".aboutme");
-let linkStyles = document.querySelector(".linkpage");
 
 //about me styling
 function addAboutStyle () {
     aboutStyle.classList.add("aboutmestyle")
-    linkStyles.classList.add("aboutmestyle")
 }
 
 function removeAboutStyle () {
-    aboutStyle.classList.remove("aboutmestyle");
-    linkStyles.classList.remove("aboutmestyle");
+    aboutStyle.classList.remove("aboutmestyle");;
 }
 
 aboutStyle.addEventListener("mouseover", addAboutStyle);
 aboutStyle.addEventListener("mouseleave", removeAboutStyle);
 
 
-linkStyles.addEventListener("mouseover", addAboutStyle);
-linkStyles.addEventListener("mouseleave", removeAboutStyle);
+//Link page events
 
+
+let divs = document.querySelector(".rym");
+
+divs.addEventListener("mouseover", highLight);
+
+function highLight () {
+    divs.classList.toggle("aboutmestyle");
+}
+
+
+//random testing
+
+let pic = document.getElementById("omakuva");
+
+function flipOver () {
+    pic.classList.add("flipped")
+}
+
+pic.addEventListener("mouseover", flipOver);
